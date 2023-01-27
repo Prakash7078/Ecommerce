423c36fb3f8dcb94e261167788aef93a78189c87
Prakash7078 <20bq1a05j5@vvit.net>
2023-01-27 14:52:54 +0530

    final commit of project

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-
+import path from 'path';
+import {dirname} from 'path';
+import { fileURLToPath } from 'url';
+const __dirname=dirname(fileURLToPath(import.meta.url));
 dotenv.config();
 const app=express()
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
@@ -25,6 +28,10 @@ app.use('/api/orders',orderRoutes);
 app.use((err,req,res,next)=>{
     res.status(500).send({message:err.message});
 });
+app.use(express.static(path.resolve(__dirname,"../frontend/build")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
+})
 
  const port=process.env.PORT || 5000;
  app.listen(port,()=>{
